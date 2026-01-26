659b1a2031e9c61e203cd58149d62f585be223f4
4npcjp7gt5-web <4npcjp7gt5@privaterelay.appleid.com>
2026-01-26 14:08:11 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://4npcjp7gt5-web.github.io',
-  output: 'static',
-  base: '/mattkindle/'
+  base: '/mattkindle/',
+  output: 'static'
 });
